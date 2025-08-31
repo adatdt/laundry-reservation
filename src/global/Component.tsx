ab88ef43197a7a5649d7c8e57a062771f72bf74e
syahadat barkah <adatdt@gmail.com>
2025-08-31 22:55:43 +0700

    first comit

---
+++ src/global/Component.tsx
@@ -0,0 +1,11 @@
+import {} from 'react'
+interface PropsBtn{
+    name:string;
+}
+export function BtnDefault(props:PropsBtn) {
+    const name = props.name
+  return (
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{name}</button>
+  )
+}
+
