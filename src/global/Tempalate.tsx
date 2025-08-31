765dee278b26e8eed6fabe279058b30a6ba1e59d
syahadat barkah <adatdt@gmail.com>
2025-08-31 23:31:16 +0700

    new commit

--- src/global/Tempalate.tsx
+++ src/global/Tempalate.tsx
@@ -1,11 +1,10 @@
-import {} from 'react'
+import React  from 'react'
 interface PropsBtn{
     name:string;
 }
-export function BtnDefault(props:PropsBtn) {
-    const name = props.name
+export const Header:React.FC<PropsBtn> = ({name}) => {
   return (
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{name}</button>
+    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{name}</button>
   )
 }
 
